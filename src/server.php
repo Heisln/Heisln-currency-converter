f979c58631aa8bc4e9bcd08eeebf7f427120b8b3
Johannes Schumeth <johannes.schumeth@gmail.com>
2021-04-08 13:50:38 +0200

    implement convert-currency request and basic authentication

--- src/server.php
+++ src/server.php
@@ -1,45 +1,80 @@
 <?php
 
+// function require_auth() {
+// 	$AUTH_USER = 'admin';
+// 	$AUTH_PASS = 'admin';
+// 	header('Cache-Control: no-cache, must-revalidate, max-age=0');
+// 	$has_supplied_credentials = !(empty($_SERVER['PHP_AUTH_USER']) && empty($_SERVER['PHP_AUTH_PW']));
+// 	$is_not_authenticated = (
+// 		!$has_supplied_credentials ||
+// 		$_SERVER['PHP_AUTH_USER'] != $AUTH_USER ||
+// 		$_SERVER['PHP_AUTH_PW']   != $AUTH_PASS
+// 	);
+// 	if ($is_not_authenticated) {
+// 		header('HTTP/1.1 401 Authorization Required');
+// 		header('WWW-Authenticate: Basic realm="Access denied"');
+// 		exit;
+// 	}
+// }
+
+$authenticated = isset($_SERVER['PHP_AUTH_USER'])
+			     && isset($_SERVER['PHP_AUTH_PW'])
+				 && $_SERVER['PHP_AUTH_USER'] == 'user'
+				 && $_SERVER['PHP_AUTH_PW'] == 'pw';
+
+if (!$authenticated) {
+    header('WWW-Authenticate: Basic realm="CurrencyConverter"');
+    header('HTTP/1.0 401 Unauthorized');
+    echo 'Text, der gesendet wird, falls der Benutzer auf Abbrechen drückt';
+	throw new Exception("Invalid username or password");
+}
+
 // turn off WSDL caching
 ini_set("soap.wsdl_cache_enabled","0");
-
-// model, which uses in web service functions as parameter
-class Book
-{
-	public $name;
-	public $year;
-}
 
 /**
  * Determines published year of the book by name.
  * @param Book $book book instance with name set.
  * @return int published year of the book or 0 if not found.
  */
-function bookYear($book)
+function convertCurrency($sourceCurrency, $targetCurrency, $value)
 {
-	// list of the books
-	$_books=[
-		['name'=>'test 1','year'=>2011],
-		['name'=>'test 2','year'=>2012],
-		['name'=>'test 3','year'=>2013],
-	];
-	// search book by name
-	foreach($_books as $bk)
-		if($bk['name']==$book->name)
-			return $bk['year']; // book found
+	$url = "http://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml";
+	$xml = new SimpleXMLElement($url, null, true);
 
-	return 0; // book not found
+	$currencyMap = array("EUR" => "1");
+
+	foreach ($xml->Cube->Cube->children() as $currencies) {
+		$currency = $currencies->attributes()->currency->__toString();
+		$rate = $currencies->attributes()->rate->__toString();
+		if(isset($currency) && isset($rate)) {
+			$currencyMap[$currency] = $rate;
+		}
+	}
+
+	if(!array_key_exists($sourceCurrency, $currencyMap)) {
+		throw new SoapFault('SOAP-ENV:Client', 'Given sourceCurrency is not supported');
+	}
+	if(!array_key_exists($targetCurrency, $currencyMap)) {
+		throw new SoapFault('SOAP-ENV:Client', 'Given targetCurrency is not supported');
+	}
+	if(!is_numeric($value)) {
+		throw new SoapFault('SOAP-ENV:Client', 'Given value is not numeric');
+	}
+
+	$sourceRate = $currencyMap[$sourceCurrency];
+	$valueInEUR = $value / $sourceRate;
+	$targetRate = $currencyMap[$targetCurrency];
+	$valueInTargetCurrency = $valueInEUR * $targetRate;
+
+	return $valueInTargetCurrency; 
 }
 
 // initialize SOAP Server
-$server=new SoapServer("test.wsdl",[
-	'classmap'=>[
-		'book'=>'Book', // 'book' complex type in WSDL file mapped to the Book PHP class
-	]
-]);
+$server=new SoapServer("test.wsdl", []);
 
 // register available functions
-$server->addFunction('bookYear');
+$server->addFunction('convertCurrency');
 
 // start handling requests
 $server->handle();
